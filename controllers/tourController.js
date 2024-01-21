4b3df0cf44c31fa8a09f4ca736f9f404ced30e4e
shane <shane@extension-interactive.com>
2024-01-21 17:45:11 +0400

    update and delete handlers

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -62,27 +62,35 @@ exports.createTour = async (req, res) => {
 };
 exports.updateTour = async (req, res) => {
   try {
-    const tour = Tour.findByIdAndUpdate(req.params.id, req.body, {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-      runValidators: true
-    })
+      runValidators: true,
+    });
     res.status(200).json({
       status: "sucess",
       data: {
-        tour
-      }
-    })
-  } catch (err)  {
+        tour,
+      },
+    });
+  } catch (err) {
     res.status(404).json({
       status: "fail",
-      message: "err"
-    })
+      message: "err",
+    });
   }
-}
+};
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: "sucess",
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "sucess",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: "err",
+    });
+  }
 };
