6e4b4c2e7cad52f6b39c97d4542877a3892d0267
shane <shane@extension-interactive.com>
2024-03-01 22:42:51 +0400

    converting natours to api only

---
+++ public/scrypt.js
@@ -0,0 +1,11 @@
+document.querySelector('.form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+
+  login(email, password)
+})
+
+const login = (email, password) => {
+    console.log(email, password)
+}
